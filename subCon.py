6c98e4265fa1e4533ee632aa6c5d0805edba05ab
Hsin-Liang Liu <hliu45@unix1.lt.ucsc.edu>
2016-10-04 21:17:22 -0700

    This method in the class store the indices of the string that starting from a word by word without repeating

---
+++ subCon.py
@@ -0,0 +1,24 @@
+#32
+class Solution(object):
+        def findSubstring(self,s,words):
+                list=[]
+                le = len(s)
+                i=0
+                while (i < le):
+                        wordlist=[]
+                        if s[i:i+3] in words:
+                                j=i
+                        while(s[i:i+3] in words and s[i:i+3] not in wordlist):
+                                wordlist.append(s[i:i+3])
+                                i+=3
+                        if len(wordlist)==len(words):
+                                list.append(j)
+                        i+=3
+                return list
+
+
+s = "barfoothefoobarman"
+words = ["foo","bar"]
+
+a=Solution()
+print a.findSubstring(s,words)
