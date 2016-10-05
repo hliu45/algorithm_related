0011ddc721242308d8ecdd7d19d4151feed23d9c
Hsin-Liang Liu <hliu45@unix1.lt.ucsc.edu>
2016-10-05 10:21:02 -0700

    When it fill out water, the the surface should not have any vally or concave. Use the properties, it re-design algorihtm to correctly calculate the amount of water that is able to fill in the tank

--- tank.py
+++ tank.py
@@ -6,32 +6,29 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+	tank = 0
 	n = len(height)
-	diff_list=[]
-	peak=[]
-	vally=[]
-	for i in range(1,n):
-		diff = height[i]-height[i-1]
-		diff_list.append(diff)
-	if diff_list[0]<0:
-		peak.append(0)
-	for j in range(1,n-1):
-		if diff_list[j]*diff_list[j-1]<0:
-			if diff_list[j]>diff_list[j-1]:
-				vally.append(j)
-			else:
-				peak.append(j)
-	if diff_list[n-2]>0:
-		peak.append(n-1)
-	tank=0
-	for i in range(1,len(peak)):
-		line=min(height[peak[i]],height[peak[i-1]])
-		for element in range(peak[i-1]+1,peak[i]):
-			tank+=(line-height[element])
-		
+	m = max(height)
+	# rev store the info. of wate up to the highest surface to the peak
+	rev = [] 
+	# the index of highest peak
+	m_index = 0 # 
+	for i in range(0,n):
+		rev.append(m-height[i])
+		if m==height[i]:
+			m_index = i
+	if m_index>0:
+		for j in range(1,m_index):
+			diff=rev[j]-rev[j-1]
+			if diff>0:
+				tank+=diff
+				rev[j]=rev[j]-diff
+				print diff
+	if m_index<n:
+		for j in range(n-1,m_index,-1):
+			diff=rev[j-1]-rev[j]
+			if diff>0:
+				tank+=diff
+				rev[j-1]-=diff
+				print diff
 	return tank
-
-height=[2,5,7,0,9,5,8,2,10,6,4,9,2,8]
-
-a=Solution()
-print a.trap(height)
