a2405c5a8d670d095397460bfbc3e6ef0155d5e6
Hsin-Liang Liu <hliu45@unix1.lt.ucsc.edu>
2016-10-05 11:41:58 -0700

    consider the case when height is empty

--- tank.py
+++ tank.py
@@ -8,20 +8,21 @@ class Solution(object):
         """
 	tank = 0
 	n = len(height)
-	# m: the peak of tank, m_index: index of that peak
-	(m,m_index) = max((v,i) for i,v in enumerate(height))
-	# rev store the info. of wate up to the highest surface to the peak
-	rev = [] 
-	for i in range(0,n):
-		rev.append(m-height[i])
-	if m_index>0:
-		for j in range(1,m_index):
-			if rev[j]-rev[j-1]>0:
-				tank+=(rev[j]-rev[j-1])
-				rev[j]=rev[j-1]
-	if m_index<n:
-		for j in range(n-1,m_index,-1):
-			if rev[j-1]-rev[j]>0:
-				tank+=(rev[j-1]-rev[j])
-				rev[j-1]-=rev[j]
+	if n > 0:
+		# m: the peak of tank, m_index: index of that peak
+		(m,m_index) = max((v,i) for i,v in enumerate(height))
+		# rev store the info. of wate up to the highest surface to the peak
+		rev = [] 
+		for i in range(0,n):
+			rev.append(m-height[i])
+		if m_index>0:
+			for j in range(1,m_index):
+				if rev[j]-rev[j-1]>0:
+					tank+=(rev[j]-rev[j-1])
+					rev[j]=rev[j-1]
+		if m_index<n:
+			for j in range(n-1,m_index,-1):
+				if rev[j-1]-rev[j]>0:
+					tank+=(rev[j-1]-rev[j])
+					rev[j-1]=rev[j]
 	return tank
